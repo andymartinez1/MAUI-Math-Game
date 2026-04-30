2e64f9e6be154f847e23d2f6c037d0d957b47eb1
Andrew Martinez <martinez.s.andrew@gmail.com>
2026-04-30 08:37:25 -0400

    Removed support for MacOS, IOS and Android for now.

--- MathGame/AppShell.xaml.cs
+++ MathGame/AppShell.xaml.cs
@@ -1,10 +1,9 @@
-﻿namespace MathGame
+﻿namespace MathGame;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
