e301d265b5b9cb0f5ce2391d7e0cfe28031fc280
Andrew Martinez <martinez.s.andrew@gmail.com>
2026-04-30 08:21:13 -0400

    Initialized MathGame project with basic structure and assets

---
+++ MathGame/GamePage.xaml.cs
@@ -0,0 +1,78 @@
+﻿namespace MathGame;
+
+public partial class GamePage : ContentPage
+{
+    private int firstNum;
+    private int score;
+    private int secondNum;
+
+    public GamePage(string gameType)
+    {
+        InitializeComponent();
+        GameType = gameType;
+        BindingContext = this;
+        CreateNewQuestion();
+    }
+
+    public string GameType { get; set; }
+
+    private void CreateNewQuestion()
+    {
+        var gameOperand = GameType switch
+        {
+            "Addition" => "+",
+            "Subtraction" => "-",
+            "Multiplication" => "*",
+            "Division" => "/",
+            _ => "",
+        };
+
+        var random = new Random();
+
+        firstNum = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
+        secondNum = GameType != "Division" ? random.Next(1, 9) : random.Next(1, 99);
+
+        if (GameType == "Division")
+            while (firstNum < secondNum || firstNum % secondNum != 0)
+            {
+                firstNum = random.Next(1, 99);
+                secondNum = random.Next(1, 99);
+            }
+
+        QuestionLabel.Text = $"{firstNum} {gameOperand} {secondNum}";
+    }
+
+    private void OnAnswerSubmitted(object? sender, EventArgs e)
+    {
+        var answer = int.Parse(AnswerEntry.Text);
+        var isCorrect = false;
+
+        switch (GameType)
+        {
+            case "Addition":
+                isCorrect = answer == firstNum + secondNum;
+                ProcessAnswer(isCorrect);
+                break;
+            case "Subtraction":
+                isCorrect = answer == firstNum - secondNum;
+                ProcessAnswer(isCorrect);
+                break;
+            case "Multiplication":
+                isCorrect = answer == firstNum * secondNum;
+                ProcessAnswer(isCorrect);
+                break;
+            case "Division":
+                isCorrect = answer == firstNum / secondNum;
+                ProcessAnswer(isCorrect);
+                break;
+        }
+    }
+
+    private void ProcessAnswer(bool isCorrect)
+    {
+        if (isCorrect)
+            score++;
+
+        AnswerLabel.Text = isCorrect ? "Correct!" : "Incorrect.";
+    }
+}
---
+++ MathGame/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MathGame;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnGameChosen(object? sender, EventArgs e)
+    {
+        var btn = (Button)sender;
+
+        Navigation.PushAsync(new GamePage(btn.Text));
+    }
+
+    private void OnViewPreviousGamesChosen(object? sender, EventArgs e)
+    {
+        Navigation.PushAsync(new PreviousGames());
+    }
+}
